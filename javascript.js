52fe1dc6079e0592fa31ed6617f57e92b5fa3b7d
matonny <onny.mat@gmail.com>
2016-11-06 22:17:39 +0100

    Working version

--- javascript.js
+++ javascript.js
@@ -1,59 +1,45 @@
 (function(){
-	addCheckSumToNumberArray();
-		function recieveNumber(){
-			var recievedNumber = prompt('Podaj liczbe:')
-			return recievedNumber
-		}
-		function numberIntoArray(){
-			var numberAsArray = recieveNumber().split('');
-			return numberAsArray
-		}
-		function loopingThroughNumbers(){
-			var numberAsArray = numberIntoArray();
-			for(i=1;i<=numberAsArray.length;i++){
-				isPositionEven(i);
-			}
-		}
-		function isPositionEven(numberPosition){
-			if(numberPosition%2===1){
-				multiplyNumber(numberIntoArray()[numberPosition-1]);
-				isMultipliedNumberSmallerThanTen(multiplyNumber(numberIntoArray()[numberPosition-1]));
-			}else{
-				increaseNumbersSum(numberIntoArray()[numberPosition-1])
-			}
-		}
-		function multiplyNumber(number){
-			var multipliedNumber = number*2
-			return multipliedNumber;
-		}
-		function isMultipliedNumberSmallerThanTen(multipliedNumber){
-			if(multipliedNumber<10){
-				increaseNumbersSum(multipliedNumber);
+'use strict'
+var number = receiveNumber();
+var digits = numberToDigitsArray(number);
+var luhnChecksum = calculateLuhnChecksum(digits);
+addChecksumToNumber(number);
+	function receiveNumber(){
+		var number = prompt('Podaj liczbe:');
+		return number;
+	}
+	function numberToDigitsArray(number){
+		var digitsArray = number.split('');
+		return digitsArray;
+	}
+	function calculateLuhnChecksum(arrayOfDigits){
+		var digitsSum = 0;
+		for(var i=0; i<arrayOfDigits.length; i++){
+			if(i%2===0){
+				if(arrayOfDigits[i]*2>=10){
+					var numberDigit = arrayOfDigits[i]*2;
+         			var arrayDoubledNumberDigit = numberToDigitsArray(numberDigit.toString());
+					var doubledNumberDigitSum = Number(arrayDoubledNumberDigit[0]) + Number(arrayDoubledNumberDigit[1]);
+					digitsSum = Number(digitsSum)+Number(doubledNumberDigitSum);			
+				}
+				else{
+
+					digitsSum = Number(digitsSum)+Number(arrayOfDigits[i]*2);
+				}
 			}
 			else{
-				multipliedNumbersDigitSum(multipliedNumber)
+				digitsSum = Number(digitsSum)+Number(arrayOfDigits[i]);
 			}
 		}
-		function multipliedNumberIntoArray(multipliedNumber){
-			var multipliedNumberAsArray = multipliedNumber.split('');
-			console.log(multipliedNumberAsArray);
-			return multipliedNumberAsArray;
+		return digitsSum%10;
+	}
+	function addChecksumToNumber(number){
+		var checksumArray = numberToDigitsArray(number);
+		if(luhnChecksum===0){
+			checksumArray.push(luhnChecksum);
+		}else{
+			checksumArray.push(10-luhnChecksum);
 		}
-		function  multipliedNumbersDigitSum(multipliedNumber){
-			var multipliedNumberArray = multipliedNumberIntoArray(multipliedNumber);
-			var digitSum = multipliedNumberArray[0] + multipliedNumberArray[1];
-			increaseNumbersSum(digitSum);
-		}
-		function increaseNumbersSum(increasingNumber){
-			var finalSum =+ increasingNumber;
-			return finalSum;
-		}
-		function countCheckSum(){
-			checkSum = increaseNumbersSum()%10
-			return checkSum;
-		}
-		function addCheckSumToNumberArray(){
-			numberIntoArray().push(countCheckSum());
-			console.log(numberIntoArray());
-		}
+		console.log(checksumArray);
+	}
 })();
